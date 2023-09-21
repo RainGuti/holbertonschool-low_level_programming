eb7157aa8c2a7c9a87d335b50c181394d0880ad5
RainGuti <7197@holbertonstudents.com>
2023-09-20 21:29:17 -0700

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,9 @@
-#!/bin/bash
-gcc puts("Programming is like building a multilingual puzzle")
+#include <stdio.h>
+/*
+ * Description: main - prints out a string to stdout.
+ * Return: 0 if success
+ */
+int main(void)
+{
+	puts(\""Programming is like building a multilingual puzzle"); return (0);
+}	
